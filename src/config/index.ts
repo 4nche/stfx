587708544160c196a9eb5f60f3aecb4e5f0634ed
anche <angelo.a@icloud.com>
2023-03-22 17:23:44 -0300

    decided to remove the tokenlist with all tokens, but only include required tokens for the assignment with scalable SVGs

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,18 +1,31 @@
-import { ChainId } from "@uniswap/sdk"
-import { TokenList,  } from "@uniswap/token-lists"
-
-const defaultTokenList: TokenList = require('@uniswap/default-token-list')
-
-const tokens = defaultTokenList.tokens
+import { ChainId } from '@uniswap/sdk';
+import { TokenList } from '@uniswap/token-lists';
 
 interface Config {
-  chain: ChainId
-  tokens: TokenList['tokens']
+  chain: ChainId;
+  tokens: TokenList['tokens'];
 }
 
 const config: Config = {
   chain: ChainId.MAINNET,
-  tokens,
-}
+  tokens: [
+    {
+      chainId: 1,
+      address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+      name: 'USD Coin',
+      symbol: 'USDC',
+      decimals: 6,
+      logoURI: process.env.PUBLIC_URL + '/icons/usdc.svg',
+    },
+    {
+      chainId: 1,
+      address: '0xc00e94cb662c3520282e6f5717214004a7f26888',
+      name: 'Compound',
+      symbol: 'COMP',
+      decimals: 18,
+      logoURI: process.env.PUBLIC_URL + '/icons/comp.svg',
+    },
+  ],
+};
 
-export default config
+export default config;
